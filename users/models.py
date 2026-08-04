4c2bd254ebce954fb3c6f211921c656bc390c525
Vahidvhd <vahidvhd91@gmail.com>
2026-08-04 03:55:15 +0100

    feat: add custom user model

---
+++ users/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        TEACHER = "teacher", "Teacher"
+        EDUCATION_OFFICER = "education_officer", "Education Officer"
+        FINANCE_OFFICER = "finance_officer", "Finance Officer"
+
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    role = models.CharField(max_length=20, choices=Role.choices)
+    phone_number = models.CharField(max_length=20, blank=True)
+    emergency_phone_number = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return self.username
