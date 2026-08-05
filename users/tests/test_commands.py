c3ac91cba821cf1c3d3c3007d0de210bec919c3c
Vahidvhd <vahidvhd91@gmail.com>
2026-08-05 22:01:12 +0100

    test: reject duplicate usernames in user command

--- users/tests/test_commands.py
+++ users/tests/test_commands.py
@@ -2,7 +2,7 @@ from unittest.mock import patch
 
 
 from django.contrib.auth import get_user_model
-from django.core.management import call_command
+from django.core.management import CommandError, call_command
 from django.test import TestCase
 
 
@@ -34,3 +34,26 @@ class CreateUserCommandTests(TestCase):
         self.assertEqual(user.phone_number, "09123456789")
         self.assertEqual(user.emergency_phone_number, "09876543211")
         self.assertTrue(user.check_password("testpass123"))
+
+    def test_duplicate_username_is_rejected(self):
+        User.objects.create_user(
+            username="teacher_test",
+            password="testpass123",
+            first_name="Teacher",
+            last_name="Test",
+            role=User.Role.TEACHER,
+            phone_number="09123456789",
+            emergency_phone_number="09876543211",
+        )
+
+        self.assertRaises(
+            CommandError,
+            call_command,
+            "create_user",
+            role=User.Role.TEACHER,
+            username="teacher_test",
+            first_name="Teacher",
+            last_name="Test",
+            phone_number="09123456789",
+            emergency_phone_number="09876543211",
+        )
