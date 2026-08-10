cbd872606a2244b19c9be851a97eb12e8e5252cb
Vahidvhd <vahidvhd91@gmail.com>
2026-08-10 14:20:46 +0100

    test: enforce course class code uniqueness

--- academics/tests/test_course_class_api.py
+++ academics/tests/test_course_class_api.py
@@ -135,3 +135,32 @@ class CourseClassAPITests(APITestCase):
         self.assertIn("session_duration", response.data)
 
 
+    def test_course_class_code_must_be_unique_per_school_and_term(self):
+        CourseClass.objects.create(
+            school=self.school,
+            term=self.term,
+            title="Python Basics",
+            class_code="PY106",
+            start_date="2026-09-01",
+            end_date="2026-12-31",
+            session_duration=90,
+        )
+
+        self.client.force_authenticate(user=self.education_officer)
+
+        response = self.client.post(
+            self.url,
+            {
+                "school": self.school.id,
+                "term": self.term.id,
+                "title": "Python Advanced",
+                "class_code": "PY106",
+                "start_date": "2026-09-01",
+                "end_date": "2026-12-31",
+                "session_duration": 90,
+            },
+        )
+
+        self.assertEqual(response.status_code, 400)
+
+
