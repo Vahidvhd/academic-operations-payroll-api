f044e9464667cf067871efa883da1ada5648bf24
Vahidvhd <vahidvhd91@gmail.com>
2026-08-12 05:04:24 +0100

    test: add teacher class assignment api test setup

---
+++ academics/tests/test_teacher_class_assignment_api.py
@@ -0,0 +1,49 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework.test import APITestCase
+
+from academics.models import CourseClass, School, TeacherClassAssignment, Term
+
+User = get_user_model()
+
+
+class TeacherClassAssignmentAPITests(APITestCase):
+    def setUp(self):
+        self.education_officer = User.objects.create_user(
+            username="education",
+            first_name="Test",
+            last_name="Education",
+            role=User.Role.EDUCATION_OFFICER,
+        )
+
+        self.teacher = User.objects.create_user(
+            username="teacher",
+            first_name="Test",
+            last_name="Teacher",
+            role=User.Role.TEACHER,
+            phone_number="07111111111",
+            emergency_phone_number="07222222222",
+        )
+
+        self.school = School.objects.create(
+            name="Test School",
+            address="London",
+        )
+
+        self.term = Term.objects.create(
+            start_date="2026-09-01",
+            end_date="2026-12-31",
+            term_type=Term.TermType.REGULAR,
+        )
+
+        self.course_class = CourseClass.objects.create(
+            school=self.school,
+            term=self.term,
+            title="Python",
+            class_code="PY101",
+            start_date="2026-09-01",
+            end_date="2026-12-31",
+            session_duration=90,
+        )
+
+        self.url = reverse("teacher-class-assignment-list")
