211ae247577b9eb7ae03c66d2cafdeca09aac5c4
Vahidvhd <vahidvhd91@gmail.com>
2026-08-12 04:33:14 +0100

    feat: add teacher class assignment routes

--- academics/urls.py
+++ academics/urls.py
@@ -1,11 +1,16 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import CourseClassViewSet, SchoolViewSet, TermViewSet
+from .views import (
+    CourseClassViewSet,
+    SchoolViewSet,
+    TeacherClassAssignmentViewSet,
+    TermViewSet,
+)
 
 router = DefaultRouter()
 
 router.register("schools", SchoolViewSet, basename="school")
 router.register("terms", TermViewSet, basename="term")
 router.register("course-classes", CourseClassViewSet, basename="course-class")
-
+router.register("teacher-class-assignments", TeacherClassAssignmentViewSet, basename="teacher-class-assignment")
 urlpatterns = router.urls
