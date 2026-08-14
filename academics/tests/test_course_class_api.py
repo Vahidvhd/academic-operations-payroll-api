f8229add5c696d6cc1623d40e55d45b286c72aa2
Vahidvhd <vahidvhd91@gmail.com>
2026-08-14 09:41:00 +0100

    test: cover missing current teacher

--- academics/tests/test_course_class_api.py
+++ academics/tests/test_course_class_api.py
@@ -1149,3 +1149,41 @@ class CourseClassAPITests(APITestCase):
                 "last_name": self.teacher.last_name,
             },
         )
+
+
+    @patch(
+    "academics.serializers.timezone.localdate",
+    return_value=date(2026, 10, 15),
+    )
+    def test_course_class_detail_returns_null_when_no_current_teacher(
+        self,
+        mocked_localdate,
+    ):
+        course_class = CourseClass.objects.create(
+            school=self.school,
+            term=self.term,
+            title="Python",
+            class_code="PY402",
+            start_date="2026-09-01",
+            end_date="2026-12-31",
+            session_duration=90,
+        )
+
+        TeacherClassAssignment.objects.create(
+            teacher=self.teacher,
+            course_class=course_class,
+            start_date="2026-09-01",
+            end_date="2026-09-30",
+        )
+
+        self.client.force_authenticate(user=self.education_officer)
+
+        url = reverse(
+            "course-class-detail",
+            args=[course_class.id],
+        )
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNone(response.data["current_teacher"])
