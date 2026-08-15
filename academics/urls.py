571da32b66e2e8084983d44e48b2dbb235d38642
Vahidvhd <vahidvhd91@gmail.com>
2026-08-15 17:08:22 +0100

    feat: add course session routes

--- academics/urls.py
+++ academics/urls.py
@@ -2,6 +2,7 @@ from rest_framework.routers import DefaultRouter
 
 from .views import (
     CourseClassViewSet,
+    CourseSessionViewSet,
     SchoolViewSet,
     TeacherClassAssignmentViewSet,
     TermViewSet,
@@ -13,4 +14,5 @@ router.register("schools", SchoolViewSet, basename="school")
 router.register("terms", TermViewSet, basename="term")
 router.register("course-classes", CourseClassViewSet, basename="course-class")
 router.register("teacher-class-assignments", TeacherClassAssignmentViewSet, basename="teacher-class-assignment")
+router.register("course-sessions", CourseSessionViewSet, basename="course-session")
 urlpatterns = router.urls
