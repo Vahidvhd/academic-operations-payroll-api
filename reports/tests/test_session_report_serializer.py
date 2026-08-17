b851e9b95e8b00d9f6e326c4141c7f615d859a23
Vahidvhd <vahidvhd91@gmail.com>
2026-08-17 17:29:13 +0100

    test: add session report serializer test setup

---
+++ reports/tests/test_session_report_serializer.py
@@ -0,0 +1,63 @@
+from datetime import date, datetime
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.utils import timezone
+from rest_framework.test import APIRequestFactory
+
+from academics.models import (
+    CourseClass,
+    CourseSession,
+    School,
+    TeacherClassAssignment,
+    Term,
+)
+from reports.serializers import SessionReportSerializer
+
+User = get_user_model()
+
+
+class SessionReportSerializerTests(TestCase):
+    def setUp(self):
+        self.teacher = User.objects.create_user(
+            username="teacher1",
+            password="testpass123",
+            role=User.Role.TEACHER,
+        )
+
+        self.school = School.objects.create(
+            name="Test School",
+            address="Test Address",
+        )
+
+        self.term = Term.objects.create(
+            start_date=date(2026, 8, 1),
+            end_date=date(2026, 8, 31),
+            term_type=Term.TermType.SUMMER,
+        )
+
+        self.course_class = CourseClass.objects.create(
+            school=self.school,
+            term=self.term,
+            course_name="Python Basics",
+            course_code="PY-101",
+            start_date=date(2026, 8, 1),
+            end_date=date(2026, 8, 31),
+            session_duration=90,
+        )
+
+        self.assignment = TeacherClassAssignment.objects.create(
+            teacher=self.teacher,
+            course_class=self.course_class,
+            start_date=date(2026, 8, 1),
+        )
+
+        self.session = CourseSession.objects.create(
+            course_class=self.course_class,
+            session_datetime=timezone.make_aware(datetime(2026, 8, 10, 10, 0)),
+            session_number=1,
+        )
+
+        self.factory = APIRequestFactory()
+        self.request = self.factory.post("/reports/")
+        self.request.user = self.teacher
