ccbcf39ee476ab027d0b00b6fc61f0ef9000ad32
Vahidvhd <vahidvhd91@gmail.com>
2026-08-18 22:16:44 +0100

    feat: add date range report filters

--- reports/filters.py
+++ reports/filters.py
@@ -7,11 +7,15 @@ class SessionReportFilter(filters.FilterSet):
     school = filters.NumberFilter(field_name="session__course_class__school")
     course_class = filters.NumberFilter(field_name="session__course_class")
     teacher = filters.NumberFilter(field_name="session__course_class__teacher_assignments__teacher", distinct=True)
-
+    date_from = filters.DateFilter(field_name="session__session_datetime", lookup_expr="date__gte")
+    date_to = filters.DateFilter(field_name="session__session_datetime", lookup_expr="date__lte")
+    
     class Meta:
         model = SessionReport
         fields = [
             "school",
             "course_class",
             "teacher",
+            "date_from",
+            "date_to",
         ]
